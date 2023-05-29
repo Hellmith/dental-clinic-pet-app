15cee2f8f0916c9bdaaffa0d0bc27d0651871865
hellfade <voidtype@outlook.com>
2023-05-29 08:55:40 +0300

    Commit

--- templates/layouts/app.template.php
+++ templates/layouts/app.template.php
@@ -113,7 +113,7 @@
                                 Персонал
                             </a>
                             <div class="py-2 first:pt-0 last:pb-0">
-                                <button type='button' class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
+                                <button type='button' class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 w-full">
                                     Новый пользователь
                                 </button>
                             </div>
