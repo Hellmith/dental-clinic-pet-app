9ce386c5c2a896e48ea849d4dae355bc5244f980
hellfade <voidtype@outlook.com>
2023-06-05 23:02:58 +0300

    Commit

--- services/setBooking.service.php
+++ services/setBooking.service.php
@@ -1,10 +1,15 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/global.inc.php';
 
+$surname = $_SESSION['USER']['patient_surname'] ?? $_POST['surname'];
+$name = $_SESSION['USER']['patient_name'] ?? $_POST['name'];
+$patronymic = $_SESSION['USER']['patient_patronymic'] ?? $_POST['patronymic'];
+$phone = $_SESSION['USER']['phone'] ?? $_POST['phone'];
+
 $result = $dataTools->setBooking(
-	$_SESSION['USER']['patient_surname'] || $_POST['surname'],
-	$_SESSION['USER']['patient_name'] || $_POST['name'],
-	$_SESSION['USER']['patient_patronymic'] || $_POST['patronymic'],
-	$_SESSION['USER']['phone'] || $_POST['phone'],
+	$surname,
+	$name,
+	$patronymic,
+	$phone,
 	$_POST['filial_id'],
 	$_POST['service_id'],
 	$_POST['doctor_id'],
